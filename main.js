8086b008782628c737f450305217b004c91e4255
Matias Garnica <garnicamatias@outlook.es>
2022-05-30 14:40:34 -0300

    a

--- main.js
+++ main.js
@@ -1,16 +1,21 @@
 
     var boton = document.getElementById('rdo');
-    var lista = document.getElementById('precio');
+    var lista = document.getElementById('lista');
     var checks = document.querySelectorAll(".valores");
     boton.addEventListener('click',function(){
         lista.innerHTML = "";
         checks.forEach((e)=>{
             if(e.checked == true){
                 var elemento = document.createElement("li");
-                elemento.className = "precio-lista-item";
+                elemento.className = "lista-item";
                 elemento.innerHTML = e.value;
                 lista.appendChild(elemento);
 
             }
         });
     });
+
+function Resultado(){
+        document.getElementById("precio").innerHTML = "El precio a pagar es de: $1.000";
+        
+}
